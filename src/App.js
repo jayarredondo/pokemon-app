e11fc86e8da24e08f4e4916e680c1117a18c28d1
Jay Arredondo <jearredondo13@gmail.com>
2021-01-05 18:57:59 -0600

    added card component and added layout styling

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import './App.css';
 
 import PokeNavbar from './components/layout/PokeNavbar'
+import Container from 'react-bootstrap/Container'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Fragment } from 'react';
+import PokeCard from './components/pokemon/PokeCard';
 
 function App() {
   return (
     <Fragment>
       <PokeNavbar/>
+      <Container>
+        <PokeCard/>
+      </Container>
     </Fragment>
   );
 }
--- src/components/layout/PokeNavbar.js
+++ src/components/layout/PokeNavbar.js
@@ -3,7 +3,7 @@ import Navbar from 'react-bootstrap/Navbar'
 
 function PokeNavbar() {
     return (
-        <Navbar bg="primary" variant="dark">
+        <Navbar bg="primary" variant="dark" className="mb-5">
             <Navbar.Brand>
             <img src="https://img.icons8.com/color/48/000000/instinct-pokemon.png"/>
                 Cyber Pokedex
