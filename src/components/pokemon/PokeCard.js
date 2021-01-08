24c91f0b6bfa478f1a552b3b3af9f3bcc62dd1e3
Jay Arredondo <jearredondo13@gmail.com>
2021-01-07 20:14:18 -0600

    did a little refactoring'

--- src/components/pokemon/PokeCard.js
+++ src/components/pokemon/PokeCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
+import PokeStats from './PokeStats'
 
 function PokeCard() {
     return (
@@ -7,10 +8,7 @@ function PokeCard() {
     <Card.Title className="pl-2 pt-1">PokeName</Card.Title>
         <Card.Img variant="top" src="http://placehold.jp/250x200.png" className="px-2 "/>
         <Card.Body>
-        <Card.Text>
-        Some quick example text to build on the card title and make up the bulk of
-        the card's content.
-        </Card.Text>
+            <PokeStats />
         {/* <Button variant="primary">Go somewhere</Button> */}
   </Card.Body>
 </Card>
---
+++ src/components/pokemon/PokeStats.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card'
+
+function PokeStats() {
+    return (
+        <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of
+        the card's content.
+        </Card.Text>
+    )
+}
+
+export default PokeStats
